2b7f06e47cfe255e8cd742faaabb606f4ab53ca3
DWIGHT2008 <mixifys33@gmail.com>
2026-05-08 02:01:14 +0300

    Add encryption key parameter to Flutterwave initialization

--- services/flutterwaveService.js
+++ services/flutterwaveService.js
@@ -2,7 +2,8 @@ const Flutterwave = require('flutterwave-node-v3');
 
 const flw = new Flutterwave(
   process.env.FLUTTERWAVE_PUBLIC_KEY,
-  process.env.FLUTTERWAVE_SECRET_KEY
+  process.env.FLUTTERWAVE_SECRET_KEY,
+  process.env.FLUTTERWAVE_ENCRYPTION_KEY
 );
 
 /**
